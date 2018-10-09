ed3b153ebdbebf34069d5e49d1b48e31d1d3cc32
Leandro Roberto <leandroroberto.br@gmail.com>
2018-10-09 08:31:51 -0300

    retorna estrutura da app api em 3.1.x

--- sapl/api/views.py
+++ sapl/api/views.py
@@ -1,18 +1,26 @@
-from datetime import datetime
 
-from django.apps import apps
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
-from django.utils.timezone import utc
+from django.http import Http404
+from django.utils.translation import ugettext_lazy as _
+from rest_framework.filters import DjangoFilterBackend
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import (IsAuthenticated, AllowAny)
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.viewsets import ReadOnlyModelViewSet
-from reversion.models import Version
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.permissions import (AllowAny, IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
+from rest_framework.viewsets import GenericViewSet
 
-from sapl.api.apps import _time_refresh_models
-from sapl.api.serializers import ModelChoiceSerializer
+from sapl.api.forms import (AutorChoiceFilterSet, AutoresPossiveisFilterSet,
+                            AutorSearchForFieldFilterSet)
+from sapl.api.serializers import (AutorChoiceSerializer, AutorSerializer,
+                                  ChoiceSerializer,
+                                  MateriaLegislativaSerializer,
+                                  ModelChoiceSerializer,
+                                  SessaoPlenariaSerializer)
+from sapl.base.models import Autor, TipoAutor
+from sapl.materia.models import MateriaLegislativa
+from sapl.sessao.models import SessaoPlenaria
+from sapl.utils import SaplGenericRelation
 
 
 class ModelChoiceView(ListAPIView):
@@ -36,91 +44,229 @@ class ModelChoiceView(ListAPIView):
         return self.model.objects.all()
 
 
-class TimeRefreshDatabaseView(APIView):
+class AutorListView(ListAPIView):
+    """
+    Listagem de Autores com filtro para autores já cadastrados
+    e/ou possíveis autores.
 
-    permission_classes = (AllowAny,)
+    - tr          - tipo do resultado
+                    Prepera Lista de Autores para 3 cenários distintos
+
+                    - default = 1
+
+                  = 1 -> para (value, text) usados geralmente
+                      em combobox, radiobox, checkbox, etc com pesquisa básica
+                      de Autores feita pelo django-filter
+                      -> processo usado nas pesquisas, o mais usado.
+
+
+                  = 3 -> Devolve instancias da classe Autor filtradas pelo
+                         django-filter
+
+    - tipo      - chave primária do Tipo de Autor a ser filtrado
+
+    - q         - busca textual no nome do Autor ou em  fields_search
+                  declarados no field SaplGenericRelation das GenericFks
+                      A busca textual acontece via django-filter com a
+                      variável `tr` igual 1 ou 3. Em caso contrário,
+                      o django-filter é desativado e a busca é feita
+                      no model do ContentType associado ao tipo.
+
+    - q_0 / q_1 - q_0 é opcional e quando usado, faz o código ignorar "q"...
+
+                  q_0 -> campos lookup a serem filtrados em qualquer Model
+                  que implemente SaplGenericRelation
+                  q_1 -> o valor que será pesquisado no lookup de q_0
+
+                  q_0 e q_1 podem ser separados por ","... isso dará a
+                  possibilidade de filtrar mais de um campo.
+
+
+                  http://localhost:8000
+                      /api/autor?tr=1&q_0=parlamentar_set__ativo&q_1=False
+                      /api/autor?tr=1&q_0=parlamentar_set__ativo&q_1=True
+                      /api/autor?tr=3&q_0=parlamentar_set__ativo&q_1=False
+                      /api/autor?tr=3&q_0=parlamentar_set__ativo&q_1=True
+
+                  http://localhost:8000
+                      /api/autor?tr=1
+                          &q_0=parlamentar_set__nome_parlamentar__icontains,
+                               parlamentar_set__ativo
+                          &q_1=Carvalho,False
+                      /api/autor?tr=1
+                          &q_0=parlamentar_set__nome_parlamentar__icontains,
+                               parlamentar_set__ativo
+                          &q_1=Carvalho,True
+                      /api/autor?tr=3
+                          &q_0=parlamentar_set__nome_parlamentar__icontains,
+                               parlamentar_set__ativo
+                          &q_1=Carvalho,False
+                      /api/autor?tr=3
+                          &q_0=parlamentar_set__nome_parlamentar__icontains,
+                               parlamentar_set__ativo
+                          &q_1=Carvalho,True
+
+
+                  não importa o campo que vc passe de qualquer dos Models
+                  ligados... é possível ver que models são esses,
+                      na ocasião do commit deste texto, executando:
+                        In [6]: from sapl.utils import models_with_gr_for_model
+
+                        In [7]: models_with_gr_for_model(Autor)
+                        Out[7]:
+                        [sapl.parlamentares.models.Parlamentar,
+                         sapl.parlamentares.models.Frente,
+                         sapl.comissoes.models.Comissao,
+                         sapl.materia.models.Orgao,
+                         sapl.sessao.models.Bancada,
+                         sapl.sessao.models.Bloco]
+
+                      qualquer atributo destes models podem ser passados
+                      para busca
+    """
+
+    TR_AUTOR_CHOICE_SERIALIZER = 1
+    TR_AUTOR_SERIALIZER = 3
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Autor.objects.all()
+    model = Autor
+
+    filter_class = AutorChoiceFilterSet
+    filter_backends = (DjangoFilterBackend, )
+    serializer_class = AutorChoiceSerializer
+
+    @property
+    def tr(self):
+        try:
+            tr = int(self.request.GET.get
+                     ('tr', AutorListView.TR_AUTOR_CHOICE_SERIALIZER))
+
+            if tr not in (AutorListView.TR_AUTOR_CHOICE_SERIALIZER,
+                          AutorListView.TR_AUTOR_SERIALIZER):
+                return AutorListView.TR_AUTOR_CHOICE_SERIALIZER
+        except:
+            return AutorListView.TR_AUTOR_CHOICE_SERIALIZER
+        return tr
 
     def get(self, request, *args, **kwargs):
-        return Response(_time_refresh_models)
+        if self.tr == AutorListView.TR_AUTOR_SERIALIZER:
+            self.serializer_class = AutorSerializer
+            self.permission_classes = (IsAuthenticated,)
+
+        if self.filter_class and 'q_0' in request.GET:
+            self.filter_class = AutorSearchForFieldFilterSet
+
+        return ListAPIView.get(self, request, *args, **kwargs)
 
 
-class TimeRefreshMobileViewSet(ReadOnlyModelViewSet):
-    permission_classes = (AllowAny,)
-    deletados = None
+class AutoresProvaveisListView(ListAPIView):
 
-    def dispatch(self, request, *args, **kwargs):
-        response = super().dispatch(request, *args, **kwargs)
-        response.data['deleted'] = self.deletados if self.deletados else []
-        return response
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Autor.objects.all()
+    model = Autor
+
+    filter_class = None
+    filter_backends = []
+    serializer_class = ChoiceSerializer
 
     def get_queryset(self):
-        return self.queryset_refresh()
+        params = {'content_type__isnull': False}
 
-    def queryset_refresh(self, queryset=None, **kwargs):
+        tipo = ''
+        try:
+            tipo = int(self.request.GET.get('tipo', ''))
+            if tipo:
+                params['id'] = tipo
+        except:
+            pass
 
-        qs = queryset if queryset else super().get_queryset()
-        opts = qs.model._meta
+        tipos = TipoAutor.objects.filter(**params)
 
-        data_min = self.request.query_params.get(
-            'data_min', kwargs.get('data_min', None))
-        data_max = self.request.query_params.get(
-            'data_max', kwargs.get('data_max', None))
-        tipo_update = self.request.query_params.get(
-            'tipo_update', kwargs.get('tipo_update', 'get'))
+        if not tipos.exists() and tipo:
+            raise Http404()
 
-        if data_min:
-            data_min = datetime.strptime(data_min, '%Y-%m-%dT%H:%M:%S.%f')
-            data_min = data_min.replace(tzinfo=utc)
-        if data_max:
-            data_max = datetime.strptime(data_max, '%Y-%m-%dT%H:%M:%S.%f')
-            data_max = data_max.replace(tzinfo=utc)
+        r = []
+        for tipo in tipos:
+            q = self.request.GET.get('q', '').strip()
 
-        if data_min or data_max:
-            if tipo_update == 'sync':
-                q_model = Q(content_type__app_label=opts.app_label,
-                            content_type__model=opts.model_name)
+            model_class = tipo.content_type.model_class()
 
-                q_data = Q()
-                if data_min:
-                    q_data &= Q(revision__date_created__gte=data_min)
+            fields = list(filter(
+                lambda field: isinstance(field, SaplGenericRelation) and
+                field.related_model == Autor,
+                model_class._meta.get_fields(include_hidden=True)))
 
-                if data_max:
-                    q_data &= Q(revision__date_created__lte=data_max)
+            """
+            fields - é um array de SaplGenericRelation que deve possuir o
+                 atributo fields_search. Verifique na documentação da classe
+                 a estrutura de fields_search.
+            """
 
-                vs = Version.objects.filter(q_model).order_by(
-                    '-revision__date_created', '-object_id')
+            assert len(fields) >= 1, (_(
+                'Não foi encontrado em %(model)s um atributo do tipo '
+                'SaplGenericRelation que use o model %(model_autor)s') % {
+                'model': model_class._meta.verbose_name,
+                'model_autor': Autor._meta.verbose_name})
 
-                vs_sync = vs.filter(q_data).values_list('object_id', flat=True)
-                vs_sync = set(map(int, vs_sync))
+            qs = model_class.objects.all()
 
-                # com o código abaixo envia todos os deletados no período sel.
-                qs = qs.filter(id__in=vs_sync)
-                qs_values = set(qs.values_list('id', flat=True))
-                self.deletados = vs_sync - qs_values
+            q_filter = Q()
+            if q:
+                for item in fields:
+                    if item.related_model != Autor:
+                        continue
+                    q_fs = Q()
+                    for field in item.fields_search:
+                        q_fs = q_fs | Q(**{'%s%s' % (
+                            field[0],
+                            field[1]): q})
+                    q_filter = q_filter & q_fs
 
-                # com código abaixo envia todos os deletados
-                # qs_values = set(qs.values_list('id', flat=True))
-                # qs = qs.filter(id__in=vs_sync)
-                # vs = vs.values_list('object_id', flat=True)
-                # vs = set(map(int, vs))
-                # self.deletados = vs - qs_values
+                qs = qs.filter(q_filter).distinct(
+                    fields[0].fields_search[0][0]).order_by(
+                        fields[0].fields_search[0][0])
+            else:
+                qs = qs.order_by(fields[0].fields_search[0][0])
 
-            elif tipo_update == 'get':
-                """
-                    se a data for datetime e o campo DateField
-                    as partes de um dia são descartadas no filtro. 
-                """
-                params = {}
-                field_to_filter_date = kwargs.get(
-                    'field_to_filter_date', self.field_to_filter_date)
+            qs = qs.values_list(
+                'id', fields[0].fields_search[0][0])
+            r += list(qs)
 
-                if data_min:
-                    params['{}__gte'.format(
-                        field_to_filter_date)] = data_min
-                if data_max:
-                    params['{}__lte'.format(
-                        field_to_filter_date)] = data_max
+        if tipos.count() > 1:
+            r.sort(key=lambda x: x[1].upper())
+        return r
 
-                qs = qs.filter(**params)
 
-        return qs
+class AutoresPossiveisListView(ListAPIView):
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Autor.objects.all()
+    model = Autor
+
+    pagination_class = None
+
+    filter_class = AutoresPossiveisFilterSet
+    serializer_class = AutorChoiceSerializer
+
+
+class MateriaLegislativaViewSet(ListModelMixin,
+                                RetrieveModelMixin,
+                                GenericViewSet):
+
+    permission_classes = (IsAuthenticated,)
+    serializer_class = MateriaLegislativaSerializer
+    queryset = MateriaLegislativa.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('numero', 'ano', 'tipo', )
+
+
+class SessaoPlenariaViewSet(ListModelMixin,
+                            RetrieveModelMixin,
+                            GenericViewSet):
+
+    permission_classes = (AllowAny,)
+    serializer_class = SessaoPlenariaSerializer
+    queryset = SessaoPlenaria.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('data_inicio', 'data_fim', 'interativa')
